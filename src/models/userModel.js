6092de510ac1ebdd3209773a05e95d61697c28e6
D0nG4667 <D0nG4667@users.noreply.github.com>
2025-04-23 18:13:30 +0300

    Add phone and email validations to model and joi schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,8 @@
 const db = require('../database/db');
 const { DataTypes } = require('sequelize')
+const validator = require('validator');
+
+
 const User = db.define(
   'User',
   {
@@ -17,13 +20,26 @@ const User = db.define(
         allowNull: false,
     },
     phone: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isValidPhone(value) {
+          if (!validator.isMobilePhone(value, 'en-NG', { strictMode: true })) {
+            throw new Error('Invalid phone number, valid Nigerian phone no format: +2348012345678');
+          }
+        }
+      }
     },
     role: {
         type: DataTypes.ENUM('landlord', 'agent', 'agency', 'renter'),
         defaultValue: 'renter',
         allowNull: false,
+        validate: {
+          isIn: {
+            args: [['landlord', 'agent', 'agency', 'renter']],
+            msg: 'Role must be one of the following: landlord, agent, agency, renter',
+          },
+        },
       },
     email: {
       type: DataTypes.STRING,
@@ -36,6 +52,12 @@ const User = db.define(
     password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: {
+            args: [6, 30],
+            msg: 'Password must be between 6 and 30 characters long',
+          },
+        },
     },
   },
 );
